fd9494afdc016303ee6872c20c8592c76ee5b309
DerekThompson80 <>
2021-05-28 14:53:43 +0000

    2nd try

--- spec/command.spec.js
+++ spec/command.spec.js
@@ -1,4 +1,4 @@
-const Command = require('../command.js');
+const Command = require('../command.js'); 
 
 // NOTE: If at any time, you want to focus on the output from a single test, feel free to comment out all the others.
 //       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
